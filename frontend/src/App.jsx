4a72f7be998b996d2556bde4e0f1301f3df0846a
BOT22345 <153290500+Nayan2234@users.noreply.github.com>
2026-02-21 22:08:04 +0530

    basic react setup, installed tailwindcss and imported the bg to assets

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-[url('./assets/bg.jpg')]">
+      <h1 className="text-sm">Code Rocket</h1>
+
+      </div>
+    </>
+  )
+}
+
+export default App
